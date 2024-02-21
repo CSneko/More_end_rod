48d08bcd514b4ec0336b8a74af4af510fcaf134f
CSneko <minecrafthetun@outlook.com>
2024-02-21 14:33:38 +0800

    完善红石末地烛

--- src/main/java/org/cneko/better_end_rod/effects/Orgasm.java
+++ src/main/java/org/cneko/better_end_rod/effects/Orgasm.java
@@ -1,10 +1,56 @@
 package org.cneko.better_end_rod.effects;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class Orgasm extends StatusEffect {
+    private long timer = 0;
+    private long damageTimer = 0;
     public Orgasm() {
         super(StatusEffectCategory.BENEFICIAL, 0XFFA500);
     }
+
+    // 这个方法在每个 tick 都会调用，以检查是否应应用药水效果
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
+    // 这个方法在应用药水效果时会被调用。
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        super.applyUpdateEffect(entity, amplifier);
+        if(entity instanceof PlayerEntity player){
+            World world = player.getWorld();
+            Random random=new Random();
+            // 每3~5秒播放一次音效
+            long time = System.currentTimeMillis();
+            if(time - timer > random.nextInt(3)+3){
+                timer = time;
+                // 随机播放0~3的音效
+                int sound = random.nextInt(4);
+                world.playSound(player,player.getBlockPos(), SoundEvent.of(new Identifier("better_end_rod","effect.orgasm."+sound)), SoundCategory.VOICE,1,1);
+            }
+            // 随机移动玩家的位置
+            int x = random.nextInt(10) - 5;
+            int z = random.nextInt(10) - 5;
+            player.move(MovementType.SHULKER_BOX,new Vec3d(x*0.001,0,z*0.001));
+            if(time - damageTimer > random.nextInt(4) +1){
+                damageTimer = time;
+                player.damage(player.getDamageSources().generic(), 0.05F);
+            }
+        }
+    }
+
+
 }
--- src/main/java/org/cneko/better_end_rod/toolItems/ElectricRod.java
+++ src/main/java/org/cneko/better_end_rod/toolItems/ElectricRod.java
@@ -5,42 +5,45 @@ import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.PlaceableOnWaterItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import org.cneko.better_end_rod.entity.InvisibleEntity;
+import net.minecraft.util.math.random.BaseRandom;
+
 
 import static org.cneko.better_end_rod.Better_end_rod.ORGASM;
 
 public class ElectricRod extends ToolItem {
-    private long timer = 0;
     public ElectricRod(ToolMaterial material, Settings settings) {
         super(material, settings);
     }
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
-        if(entity instanceof PlayerEntity player && selected && player.getOffHandStack().getItem() == this.asItem()){
-            // 当持有对象是玩家且玩家选择了该物品并且在副手
+        if(entity instanceof PlayerEntity player && player.getOffHandStack().getItem() == this.asItem()){
+            // 当持有对象是玩家且物品在副手
             // 添加高潮效果
-            StatusEffectInstance orgasm = new StatusEffectInstance(ORGASM,5,1);
+            StatusEffectInstance orgasm = new StatusEffectInstance(ORGASM,300,1);
             player.addStatusEffect(orgasm);
+            // 1/100的概率发送文本
+            if(new java.util.Random().nextInt(100) == 0){
+                // 生成0~5的随机数
+                int i = new java.util.Random().nextInt(5);
+                player.sendMessage(Text.translatable("message.better_end_rod.electric_rod.using."+i));
+            }
             // 设置玩家动作
             player.setPose(EntityPose.SWIMMING);
             // 播放粒子效果
             world.addParticle(ParticleTypes.END_ROD,player.getX(),player.getY(),player.getZ(),0,0,0);
-            // 每3秒播放一次音效
-            long time = System.currentTimeMillis();
-            if(time - timer > 3){
-                timer = time;
-                world.playSound(player,player.getBlockPos(), SoundEvent.of(new Identifier("better_end_rod","electric_rod.use")), SoundCategory.VOICE,1,1);
+            if(player instanceof ServerPlayerEntity serverPlayer) {
+                stack.damage(1, Random.create(), serverPlayer);
             }
+            player.setStackInHand(Hand.OFF_HAND,stack);
         }
     }
 }
