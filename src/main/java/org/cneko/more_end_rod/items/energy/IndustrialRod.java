edf5acf5ff92605ded88cda4938be2f5f13a9c29
CSneko <minecrafthetun@outlook.com>
2024-06-29 16:48:24 +0800

    完~善~工~业~末~地~烛~ ( • ̀ω•́ )

--- src/main/java/org/cneko/more_end_rod/items/energy/IndustrialRod.java
+++ src/main/java/org/cneko/more_end_rod/items/energy/IndustrialRod.java
@@ -1,23 +1,28 @@
 package org.cneko.more_end_rod.items.energy;
 
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.cneko.more_end_rod.items.tools.ElectricRod;
+import org.jetbrains.annotations.Nullable;
 import team.reborn.energy.api.EnergyStorage;
 import team.reborn.energy.api.base.SimpleEnergyItem;
 
+import java.util.List;
+
 public class IndustrialRod extends ElectricRod implements SimpleEnergyItem {
     public long CAPACITY = 10^5;
-    public static final int MAX_INSERT = 1000;
-    public static final int MAX_EXTRACT = 1000;
+    public static final int MAX_INSERT = 10^5;
+    public static final int MAX_EXTRACT = 10^5;
     public IndustrialRod() {
         super(IndustrialRodMaterial.INSTANCE, new Settings().maxCount(1));
     }
@@ -47,8 +52,27 @@ public class IndustrialRod extends ElectricRod implements SimpleEnergyItem {
         // 将减少耐久改为减少电量
         EnergyStorage e = EnergyStorage.ITEM.find(stack, ContainerItemContext.ofPlayerHand(player, Hand.OFF_HAND));
         if (e != null) {
-            this.setStoredEnergy(stack, e.getAmount() - amount);
+            this.setStoredEnergy(stack, e.getAmount() - amount* 10L);
         }
+        // 将剩下的电量设置为显示耐久
+        if (e != null) {
+            stack.setDamage((int)(100 - e.getAmount() / this.getEnergyCapacity(stack)));
+        }
+    }
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        try {
+            // 获取电量
+            long amount = this.getStoredEnergy(stack);
+            // 将电量转换为 k 为单位
+            amount = amount / 1000;
+            tooltip.add(Text.translatable("item.more_end_rod.universal.energy", amount + "k", this.getEnergyCapacity(stack) / 1000 + "k"));
+        }catch (Exception ignored){}
+    }
+    @Override
+    public void setStoredEnergy(ItemStack stack, long amount) {
+        SimpleEnergyItem.setStoredEnergyUnchecked(stack, amount);
+        stack.setDamage((int)(100 - amount / this.getEnergyCapacity(stack)));
     }
 
 
@@ -56,7 +80,7 @@ public class IndustrialRod extends ElectricRod implements SimpleEnergyItem {
         public static IndustrialRodMaterial INSTANCE = new IndustrialRodMaterial();
         @Override
         public int getDurability() {
-            return 2000;
+            return 100;
         }
 
         @Override
